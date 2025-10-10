c614dd982a1f4ddeaf20dcb7b80e030899a43a75
harsh-dev-ux <harshhajare77@gmail.com>
2025-10-10 20:49:48 +0530

    tetsing commit message     # Please enter the commit message for your changes. Lines starting     # with '#' will be ignored, and an empty message aborts the commit.     #     # On branch main     # Your branch is up to date with 'origin/main'.     #     # Changes to be committed:     #	new file:   main.c     #

---
+++ main.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stdbool.h>
+
+int main(){
+
+
+    //FIXME: Right align numbers with a width of 3
+
+   
+    int num1 = 1;
+    int num2 = 10;
+    int num3 = 100;
+    int num4 = 1000;
+
+
+    printf("%3d\n", num1);
+    printf("%3d\n", num2);
+    printf("%3d\n", num3);
+    printf("%3d\n", num4);
+
+    return 0;
+
+
+    
+
+
+
+
+
+    }
+
+
+
+
+    
+
