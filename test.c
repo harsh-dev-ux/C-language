82d4d2f30ebb01a83a4c8671747357dd23424246
harsh-dev-ux <harshhajare77@gmail.com>
2025-11-08 22:53:35 +0530

    # Please enter the commit message for your changes. Lines starting     # with '#' will be ignored, and an empty message aborts the commit.     #     # On branch main     # Your branch is up to date with 'origin/main'.     #     # Changes to be committed:     #	modified:   test.c     #     # Changes not staged for commit:     #	modified:   test.c     #     # Untracked files:     #	5_weight_calculator_program.c     #	6_temperature_program.c     #

--- test.c
+++ test.c
@@ -1,10 +1,19 @@
 #include <stdio.h>
-#include <bool.h>
+#include <stdbool.h>
 
 int main() {
 
 
+        bool istudent = true ;
         
+        if (issdtudent ==true) { 
+            printf("your are a student\n");
+
+        }
+
+        else{
+            printf("your are not a student\n");
+        }
 
 
 
