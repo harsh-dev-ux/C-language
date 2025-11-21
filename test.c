29a9d2d83d503840b855528a66d234ffebef4a1e
harsh-dev-ux <harshhajare77@gmail.com>
2025-11-21 19:08:18 +0530

    # Please enter the commit message for your changes. Lines starting     # with '#' will be ignored, and an empty message aborts the commit.     #     # On branch main     # Your branch is up to date with 'origin/main'.     #     # Changes to be committed:     #	new file:   12_digital_clock_program.c     #	modified:   test.c     #

--- test.c
+++ test.c
@@ -1,44 +1,28 @@
 #include <stdio.h>
-#include <string.h>
-
 
 
 
 int main(){ 
 
-    char names[3][25] = {0};
+    int age = 25;
+
+    printf("%p", &age);
+
+
+
+
+
+
+
+
+
+
+
+
+    return 0; } 
     
-    
+
    
-
-    for(int i =0; i <3 ; i++){
-         printf("Enter a name: ");
-    fgets(names[i], sizeof(names[i]), stdin);
-    names[i][strlen(names[i]) - 1] = '\0';
-
-    }
-
-    for(int i =0; i<3 ; i++){
-        printf("%s\n", names[i]);
-
-
-    }
-
-
-    
-
-  
-  
-
-
-
-
-
-
-return 0;
-    }
-
-
 
    
 
